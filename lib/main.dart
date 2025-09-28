4fd024b861ff35ef2fec6bad49aa1282b275e2ab
Matheus Silva <msantcs99@gmail.com>
2025-09-28 13:09:17 -0300

    feat: adding multi bloc provider to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_cubit/cubits/cubits.dart';
 import 'package:todo_cubit/pages/todo_page.dart';
 
 void main() {
@@ -10,11 +12,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: TodoPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TodoFilterCubit>(create: (context) => TodoFilterCubit()),
+        BlocProvider<TodoSearchCubit>(create: (context) => TodoSearchCubit()),
+        BlocProvider<TodoListCubit>(create: (context) => TodoListCubit()),
+        BlocProvider<ActiveTodoCountCubit>(
+          create: (context) => ActiveTodoCountCubit(
+            todoListCubit: BlocProvider.of<TodoListCubit>(context),
+          ),
+        ),
+        BlocProvider<FilteredTodosCubit>(
+          create: (context) => FilteredTodosCubit(
+            todoFilterCubit: BlocProvider.of<TodoFilterCubit>(context),
+            todoSearchCubit: BlocProvider.of<TodoSearchCubit>(context),
+            todoListCubit: BlocProvider.of<TodoListCubit>(context),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Todo w/ Cubit',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.orange),
+        home: TodoPage(),
+      ),
     );
   }
 }
