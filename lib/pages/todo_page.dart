967cdaaad08a1c957b9dc01968d514ebcceb6a46
Matheus Silva <msantcs99@gmail.com>
2025-10-06 23:38:04 -0300

    feat: adding header component

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:todo_cubit/widgets/todo_header.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('TODOS')));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 20, vertical: 40),
+            child: Column(children: [TodoHeader()]),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/todo_header.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_cubit/cubits/cubits.dart';
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'To-Do',
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          '${context.watch<ActiveTodoCountCubit>().state.activeTodoCount} items left',
+          style: TextStyle(fontSize: 20, color: Colors.black),
+        ),
+      ],
+    );
+  }
+}
